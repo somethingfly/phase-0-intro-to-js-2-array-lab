f4f6366e36793daf417724c1bb695a4e10b4022b
Alfred <somethingfly@somethingfly.com>
2022-06-18 19:14:07 -0700

    commit tree 5d8e0a599101aacf970b00bef090c774c7a10400

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(name){
+    const newCats = cats.slice(0,-1);
+    return newCats;
+}
+
+function removeFirstCat(name){
+    const newCats = cats.slice(1);
+    return newCats;
+}
